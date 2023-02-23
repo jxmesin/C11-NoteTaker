8f68921d77e6c8875298eefab4f511dc1493ffe9
james <arvinjamesinanoria@gmail.com>
2023-02-22 18:44:41 -0800

    server made

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+
+
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+
+app.use (express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server is ready on port ${PORT}!`);
+});
