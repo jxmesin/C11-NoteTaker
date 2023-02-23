a88f77093f1683cc3b661afc3f70a3e37a5822a8
james <arvinjamesinanoria@gmail.com>
2023-02-22 18:53:19 -0800

    test

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
@@ -8,12 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 
-
-
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
-
 
 app.use (express.static('public'));
 app.use('/api', apiRoutes);
